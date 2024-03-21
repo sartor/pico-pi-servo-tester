4ddcecd4128b564bb3de78b7a35b8027acf338cd
Sartor <sartorua@gmail.com>
2024-03-22 00:56:00 +0200

    WS2812 led indicator

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@
 
 use core::sync::atomic::{AtomicBool, AtomicUsize, Ordering};
 use defmt::*;
-use embassy_rp::{bind_interrupts, gpio:: {Pin, AnyPin, Level, Output, Pull}, adc, pwm, peripherals};
+use embassy_rp::{bind_interrupts, gpio::Pull, adc, pwm, peripherals, pio};
 use embassy_sync::blocking_mutex::raw::ThreadModeRawMutex;
 use embassy_sync::mutex::Mutex;
 use embassy_time::Timer;
@@ -12,6 +12,7 @@ use {defmt_rtt as _, panic_probe as _};
 use crate::buttons::Button;
 
 mod buttons;
+mod pio_ws2812;
 
 static MODES: [[u16; 2]; 3] = [
     [1000, 2000],
@@ -24,6 +25,7 @@ static CURRENT_SIDE_INDEX: AtomicUsize = AtomicUsize::new(0);
 static RUNNING: AtomicBool = AtomicBool::new(true);
 bind_interrupts!(struct Irqs {
     ADC_IRQ_FIFO => adc::InterruptHandler;
+    PIO0_IRQ_0 => pio::InterruptHandler<peripherals::PIO0>;
 });
 
 type PwmType = Mutex<ThreadModeRawMutex, Option<pwm::Pwm<'static, peripherals::PWM_CH5>>>;
@@ -45,31 +47,46 @@ async fn main(spawner: embassy_executor::Spawner) {
         *(PWM_CONFIG.lock().await) = Some(c);
     }
 
-    spawner.spawn(run_led(p.PIN_25.degrade())).unwrap();
+    let pio::Pio { mut common, sm0, .. } = pio::Pio::new(p.PIO0, Irqs);
+
+    let ws2812 = pio_ws2812::Ws2812::new(&mut common, sm0, p.DMA_CH0, p.PIN_16);
+
+    spawner.spawn(run_main(ws2812)).unwrap();
     spawner.spawn(run_adc(p.PIN_26, p.ADC)).unwrap();
     spawner.spawn(run_pwm()).unwrap();
 }
 
 #[embassy_executor::task]
-async fn run_led(led_pin: AnyPin) {
-    let mut led = Output::new(led_pin, Level::Low);
+async fn run_main(mut ws2812: pio_ws2812::Ws2812<'static, peripherals::PIO0, 0, 1>) {
+
+    let mut color;
+    let mut last_pwm = 0;
 
     loop {
         update_pwm().await;
 
-        led.set_high();
-        Timer::after_millis(50).await;
+        let mode = CURRENT_MODE_INDEX.load(Ordering::Relaxed);
+        let side = CURRENT_SIDE_INDEX.load(Ordering::Relaxed);
+        let pwm_value = MODES[mode][side];
 
-        if RUNNING.load(Ordering::Relaxed) {
-            led.set_low();
-            Timer::after_millis(50).await;
+        if last_pwm != pwm_value {
+            debug!("PWM: {}", pwm_value);
         }
+
+        if !RUNNING.load(Ordering::Relaxed) {
+            color = pio_ws2812::color_gray()
+        } else {
+            color = pio_ws2812::color_wheel(((pwm_value - 500) / 10) as u8);
+        }
+
+        ws2812.write(&[color]).await;
+        Timer::after_millis(50).await;
+        last_pwm = pwm_value;
     }
 }
 
 #[embassy_executor::task]
 async fn run_adc(adc_pin: peripherals::PIN_26, p_adc: peripherals::ADC) {
-
     let mut adc = adc::Adc::new(p_adc, Irqs, adc::Config::default());
     let mut pin = adc::Channel::new_pin(adc_pin, Pull::Up);
 
@@ -78,10 +95,10 @@ async fn run_adc(adc_pin: peripherals::PIN_26, p_adc: peripherals::ADC) {
         let cur_mode = CURRENT_MODE_INDEX.load(Ordering::Relaxed);
         match button {
             Button::Up => {
-                CURRENT_MODE_INDEX.store(if cur_mode == 0 { 0 } else {cur_mode - 1}, Ordering::Relaxed)
+                CURRENT_MODE_INDEX.store(if cur_mode == 0 { 0 } else { cur_mode - 1 }, Ordering::Relaxed)
             }
             Button::Down => {
-                CURRENT_MODE_INDEX.store(if cur_mode == 2 { 2 } else {cur_mode + 1} , Ordering::Relaxed)
+                CURRENT_MODE_INDEX.store(if cur_mode == 2 { 2 } else { cur_mode + 1 }, Ordering::Relaxed)
             }
             Button::Left => {
                 CURRENT_SIDE_INDEX.store(1, Ordering::Relaxed)
